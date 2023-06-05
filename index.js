a66719ef348f49fbb8e048343fd3610947db17f5
Krasimir <krasimir.st.tsonev@gmail.com>
2023-06-05 10:21:47 +0300

    Wrapping up the implementation

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ function parse(obj) {
   
   let res = {};
   Object.keys(obj).forEach(key => {
-    const value = obj[key];
+    let value = obj[key];
     if (typeof value === 'string') {
-      if (value.match(/^\!/)) {
-        res[key] = { $ne: value.replace(/^\!/, '') };
-      } else {
-        res[key] = { $eq: value };
+      res[key] = parseStringValue(value);
+    } else if (Array.isArray(value)) {
+      if (key.match(/\!\[\]$/)) {
+        key = key.replace(/\!\[\]$/, '');
+        res[key] = { $nin: value };
+      } else if (key.match(/\[\]$/)) {
+        key = key.replace(/\[\]$/, '');
+        res[key] = { $in: value };
+      } else if (key.match(/\[or\]$/)) {
+        key = key.replace(/\[or\]$/, '');
+        res['$or'] = value.map(v => ({ [key]: parseStringValue(v) }));
+      } else if (key.match(/\[and\]$/)) {
+        key = key.replace(/\[and\]$/, '');
+        res['$and'] = value.map(v => ({ [key]:  parseStringValue(v) }));
+      } else if (key.match(/\[nor\]$/)) {
+        key = key.replace(/\[nor\]$/, '');
+        res['$nor'] = value.map(v => ({ [key]:  parseStringValue(v) }));
       }
     }
   });
@@ -16,6 +29,47 @@ function parse(obj) {
   return res;
 }
 
+function parseStringValue(value) {
+  if (value.match(/^\!=/)) {
+    return { $exists: false };
+  } else if (value.match(/^\!/)) {
+    value = value.replace(/^\!/, '');
+    return { $ne: value };
+  } else if (value.match(/^=/)) {
+    return { $exists: true };
+  } else if (value.match(/^>=/)) {
+    value = value.replace(/^>=/, '');
+    if (isNumeric(value)) {
+      return { $gte: parseFloat(value) };
+    }
+  } else if (value.match(/^>/)) {
+    value = value.replace(/^>/, '');
+    if (isNumeric(value)) {
+      return { $gt: parseFloat(value) };
+    }
+  } else if (value.match(/^<=/)) {
+    value = value.replace(/^<=/, '');
+    if (isNumeric(value)) {
+      return { $lte: parseFloat(value) };
+    }
+  } else if (value.match(/^</)) {
+    value = value.replace(/^</, '');
+    if (isNumeric(value)) {
+      return { $lt: parseFloat(value) };
+    }
+  } else if (value.match(/^\$/)) {
+    value = value.replace(/^\$/, '');
+    return { $regex: new RegExp(value), $options: 'i' };
+  } else {
+    return { $eq: value };
+  }
+}
+
 module.exports = {
   parse
+}
+
+/* ------------------------------------------------------------------------ utils */
+function isNumeric(value) {
+  return !isNaN(value - parseFloat(value));
 }
--- test.js
+++ test.js
@@ -1,6 +1,34 @@
 const assert = require('assert');
+const qs = require('querystring');
 
 const { parse } = require('./index.js');
+
+const CASES = [
+
+  ['$eq', 'foo=bar', { foo: 'bar' }, { foo: { $eq: 'bar' } }],
+  ['$ne', 'foo=!bar', { foo: '!bar' }, { foo: { $ne: 'bar' } }],
+  ['$exists', 'foo==bar', { foo: '=bar' }, { foo: { $exists: true } }],
+  ['$exists', 'foo=!=bar', { foo: '!=bar' }, { foo: { $exists: false } }],
+  ['$gt', 'foo=>20', { foo: '>20' }, { foo: { $gt: 20 } }],
+  ['$gte', 'foo=>=20', { foo: '>=20' }, { foo: { $gte: 20 } }],
+  ['$lt', 'foo=<20', { foo: '<20' }, { foo: { $lt: 20 } }],
+  ['$lte', 'foo=<=20', { foo: '<=20' }, { foo: { $lte: 20 } }],
+  ['$in', 'foo[]=a&foo[]=b', { 'foo[]': [ 'a', 'b' ] }, { foo: { $in: [ 'a', 'b' ] } }],
+  ['$nin', 'foo![]=a&foo![]=b', { 'foo![]': [ 'a', 'b' ] }, { foo: { $nin: [ 'a', 'b' ] } }],
+  ['$or', 'foo[or]=a&foo[or]=b', { 'foo[or]': [ 'a', 'b' ] },
+    { $or: [ { foo: { $eq: 'a' }}, { foo: { $eq: 'b' }} ] }
+  ],
+  ['$or', 'foo[or]=$bar&foo[or]=>20', { 'foo[or]': [ '$bar', '>20' ] },
+    { $or: [ { foo: { $regex: new RegExp('bar'), $options: 'i' }}, { foo: { $gt: 20 }} ] }
+  ],
+  ['$and', 'foo[and]=a&foo[and]=b', { 'foo[and]': [ 'a', 'b' ] },
+    { $and: [ { foo: { $eq: 'a' }}, { foo: { $eq: 'b' }} ] }
+  ],
+  ['$nor', 'foo[nor]=a&foo[nor]=b', { 'foo[nor]': [ 'a', 'b' ] },
+    { $nor: [ { foo: { $eq: 'a' }}, { foo: { $eq: 'b' }} ] }
+  ],
+  ['$regex', 'foo=$bar', { foo: '$bar' }, { foo: { $regex: new RegExp('bar'), $options: 'i' } }],
+]
 
 describe('Passing falsy or non object values', () => {
   it('should return empty object', () => {
@@ -11,14 +39,12 @@ describe('Passing falsy or non object values', () => {
     assert.deepStrictEqual(parse(5), {});
     assert.deepStrictEqual(parse([]), {});
   });
-  [
-
-    ['$eq', { foo: 'bar' }, { foo: { $eq: 'bar' } }],
-    ['$ne', { foo: '!bar' }, { foo: { $ne: 'bar' } }],
-    ['$exists', { foo: '=bar' }, { foo: { $exists: 'bar' } }],
-
-  ].forEach(([ description, input, output ]) => {
-    it(`${description} - ${JSON.stringify(input)} -> ${JSON.stringify(output)}`, () => {
+});
+describe('Passing working values', () => {
+  CASES.forEach(([ description, queryString, input, output ]) => {
+    it(`${description} - ${queryString} - ${JSON.stringify(input)} -> ${JSON.stringify(output)}`, () => {
+      const qsObj = JSON.parse(JSON.stringify(qs.parse(queryString)))
+      assert.deepStrictEqual(qsObj, input);
       assert.deepStrictEqual(parse(input), output);
     });
   });
